74e2292fffb6393e35306ef52d85c72afc109ecb
bronek14 <116382641+bronek14@users.noreply.github.com>
2024-09-09 18:15:42 -0500

    Update environment.ts with firebaseConfig data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,31 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 
 export const environment = {
-  production: false
+  production: false,
+  //---------------------------------------------ADDED FOR FIREBASE TESTING---------------------------------------------------------------------------//
+
+  firebaseConfig:{
+  apiKey: "AIzaSyAAfMAgiwXgap0kmkv9O_Ehj9KGhVKyFMk",
+  authDomain: "arthive2024.firebaseapp.com",
+  projectId: "arthive2024",
+  storageBucket: "arthive2024.appspot.com",
+  messagingSenderId: "975354135360",
+  appId: "1:975354135360:web:fafb2df9b0de74b37370a5",
+  measurementId: "G-MPM1V6QEM6"
+  //---------------------------------------------ADDED FOR FIREBASE TESTING---------------------------------------------------------------------------//
+
+  }
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 };
 
 /*
