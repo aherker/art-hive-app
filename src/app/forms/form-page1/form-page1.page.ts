fcfce5766acda85676de04b25a1cb9f0e6d3f210
nsantil <nsanti320@gmail.com>
2024-10-22 11:36:44 -0500

    added the questions to the form to match them with their corresponding lanswer

--- src/app/forms/form-page1/form-page1.page.ts
+++ src/app/forms/form-page1/form-page1.page.ts
@@ -18,58 +18,103 @@ export class FormPage1Page implements OnInit {
   ngOnInit() {
     this.artHiveQuestionare = this.formBuilder.group({
       membersName: this.formBuilder.array([]),
+      startDateLabel: ['Start date'],
       startDate: ['', Validators.required],
+      endDateLabel: ['End date'],
       endDate: ['', Validators.required],
+      locationNameLabel: ['Name'],
       locationName: ['', Validators.required],
+      streetLabel: ['Street'],
       street: ['', Validators.required],
+      cityLabel: ['City'],
       city: ['', Validators.required],
+      stateLabel: ['State'],
       state: ['', Validators.required],
+      zipLabel: ['ZIP'],
       zip: ['', [Validators.required, Validators.pattern('[0-9]{5}')]],
       contactList: this.formBuilder.array([]),
       partnerList: this.formBuilder.array([]),
       facilitatorList: this.formBuilder.array([]),
+      numParticipantsLabel: ['Number of participants (include facilitators)'],
       numParticipants: ['', Validators.required],
+      numSeniorsLabel: ['Approximate number of older adults >= 65'],
       numSeniors: ['', Validators.required],
       numStudentsList: this.formBuilder.array([]),
+      numChildrenLabel: ['Number of children'],
       numChildren: ['', Validators.required],
+      numNewParticipantsLabel: ['Number of new participants'],
       numNewParticipants: ['', Validators.required],
+      DiscoveryMethodZero: ['Word of mouth'],
+      DiscoveryMethodOne: ['Passing by and being curious'],
+      DiscoveryMethodTwo: ['Social Media'],
       discoveryMethods: this.formBuilder.array([
         this.formBuilder.group({ checked: [false] }), // Word of mouth
         this.formBuilder.group({ checked: [false] }), // Passing by
         this.formBuilder.group({ checked: [false] }), // Social media
       ]),
+      otherDiscoverylabel: ['If other, please specify'],
       otherDiscovery: [''],
+      EDIQuestionsLabel: ['Were there any discussions related to EDI topics (age, gender, sexual orientation, ethnicity/race, disability, religion, neighborhood, etc.)?'],
       EDIQuestions: [''],
+      selfIDLabel: ['If self-identification occurred, what may have facilitated it?'],
       selfID: [''],
+      commonGroundLabel: ['If needed, was a common ground found? If so, how (shared goals, community projects, etc.)?'],
       commonGround: [''],
+      underRepresentedPerspectivesLabel: ['What could be done to reach out for under-represented perspectives? (If any suggestions from the group)'],
       underRepresentedPerspectives: [''],
+      underRepresentedPerspectivesReachOutLabel: ['What could be done to reach out for under-represented perspectives? (If any suggestions from the group)'],
       underRepresentedPerspectivesReachOut: [''],
       formsOfExpressionsList: this.formBuilder.array([]),
+      themesAndSymbolsLabel: ['Themes & symbols (please do not write techniques and art materials here)'],
       themesAndSymbols: [''],
       materialsUsedList: this.formBuilder.array([]),
+      selectedETCLabel: ['Expressive Therapies Continuum (ETC)'],
       selectedETC: ['', Validators.required],
+      discussionCommunityLabel: ['Discussion Theme: related to community'], 
       discussionCommunity: [''],
+      discussionArtmakingLabel: ['Discussion themes: related to artmaking'],
       discussionArtmaking: [''],
+      discussionSelfCareLabel: ['Discussion themes: self-care and personal successes'],
       discussionSelfCare: [''],
+      discussionChallengesLabel: ['Discussion themes: challenges'],
       discussionChallenges: [''],
+      discussionOtherLabel: ['Discussion themes: other'],
       discussionOther: [''],
+      highlightsSpaceLabel: ['Highlights: holding the physical/digital space'],
       highlightsSpace: [''],
+      highlightsCommunityLabel: ['Highlights: community experience'],
       highlightsCommunity: [''],
+      highlightsEnvironmentLabel: ['Highlights: accessible, third-space studio environment'],
       highlightsEnvironment: [''],
+      highlightsLeadershipLabel: ['Highlights: participants\' leadership (e.g., participants taking lead in explaining a technique to the group - or offering to do a skill share, or an idea about a community activity they want to start, participants being ambassadors for our activities, etc.)'],
       highlightsLeadership: [''],
+      highlightsBoundariesLabel: ['Highlights: boundaries'],
       highlightsBoundaries: [''],
+      highlightsOtherLabel: ['Highlights: other'],
       highlightsOther: [''],
+      challengesSpaceLabel: ['Challenges: holding the physical/digital space'],
       challengesSpace: [''],
+      challengesCommunityLabel: ['Challenges: community experience'],
       challengesCommunity: [''],
+      challengesArtmakingLabel: ['Challenges: artmaking'],
       challengesArtmaking: [''],
+      challengesEnvironmentLabel: ['Challenges: accessible, third space studio environment'],
       challengesEnvironment: [''],
+      challengesLeadershipLabel: ['Challenges: participants\' leadership'],
       challengesLeadership: [''],
+      challengesBoundariesLabel: ['Challenges: boundaries'],
       challengesBoundaries: [''],
+      challengesOtherLabel: ['Challenges: other'],
       challengesOther: [''],
+      circleofCareLabel: ['Circles of Care connections (e.g., friendships forming between participants, participant(s) sharing resources of self-care activities with the group, participants opening discussions about their circle of care, etc.)'],
       circleOfCare: [''],
+      testimoniesLabel: ['Testimonies - direct quotes from individuals - please do not write down names'],
       testimonies: [''],
+      proposedThemesLabel: ['Proposed themes/interests from participants'],
       proposedThemes: [''],
+      actionItemsLabel: ['Action items required and who will follow up'],
       actionItems: [''],
+      researchQuestionsLabel: ['Potential research questions'],
       researchQuestions: ['']
     });
 
@@ -117,7 +162,12 @@ export class FormPage1Page implements OnInit {
   // }
 
   addMember() {
-    this.membersName.push(this.formBuilder.control('', Validators.required));
+    this.membersName.push(
+      this.formBuilder.group({
+        membersNameLabel: ['Member\'s name'],
+        membersNameInput: ['', Validators.required]
+      })
+    );
   }
 
   removeMember(index: number) {
@@ -127,8 +177,11 @@ export class FormPage1Page implements OnInit {
   addContact() {
     this.contactList.push(
       this.formBuilder.group({
+        contactNameLabel: ['Name'],
         contactName: ['', Validators.required],
+        contactEmailLabel: ['Email'],
         contactEmail: ['', [Validators.required, Validators.email]],
+        contactPhoneLabel: ['Phone number'],
         contactPhone: ['', [Validators.required, Validators.pattern('^([0-9]{3})[-]?([0-9]{3})[-]?([0-9]{4})$')]]
       })
     );
@@ -139,7 +192,12 @@ export class FormPage1Page implements OnInit {
   }
 
   addPartner() {
-    this.partnerList.push(this.formBuilder.control('', Validators.required));
+    this.partnerList.push(
+      this.formBuilder.group({
+        partnerNameLabel: ['Partner\'s name'],
+        partnerNameInput: ['', Validators.required]
+      })
+    );
   }
 
   removePartner(index: number) {
@@ -147,7 +205,12 @@ export class FormPage1Page implements OnInit {
   }
 
   addFacilitator() {
-    this.facilitatorList.push(this.formBuilder.control('', Validators.required));
+    this.facilitatorList.push(
+      this.formBuilder.group({
+        facilitatorListLabel: ['Facilitator\'s name'],
+        facilitatorListInput: ['', Validators.required]
+      })
+    );
   }
 
   removeFacilitator(index: number) {
@@ -156,7 +219,9 @@ export class FormPage1Page implements OnInit {
 
   addInstitution() {
     this.numStudentsList.push(this.formBuilder.group({
+      numStudentsLabel: ['Numbers of students'],
       numStudents: ['', Validators.required],
+      eduInstitutionLabel: ['Educational institution'],
       eduInstitution: ['', Validators.required],
     }));
   }
@@ -169,7 +234,9 @@ export class FormPage1Page implements OnInit {
 
   addExpression(){
     this.formsOfExpressionsList.push(this.formBuilder.group({
+      formsOfExpressionsLabel: ['Form of art'],
       formsOfExpressionType: ['', Validators.required],
+      numOfExpressionLabel: ['Number of art form'],
       numOfExpression: ['', Validators.required]
     }));
   }
@@ -182,7 +249,9 @@ export class FormPage1Page implements OnInit {
 
   addMaterialsUsed(){
     this.materialsUsedList.push(this.formBuilder.group({
+      materialsUsedLabel: ['Materials or instruments used'],
       materialsUsedType: ['', Validators.required],
+      numMaterialsUsedLabel: ['Number materials or instruments used'],
       numMaterialsUsed: ['', Validators.required]
     }));
   }
